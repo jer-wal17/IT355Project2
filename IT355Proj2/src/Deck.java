34e20bfb22246f4447aea6bc70e9fa518b226183
Ultibat <jeromew1714@gmail.com>
2025-04-02 13:39:40 -0500

    Update Deck.java

--- IT355Proj2/src/Deck.java
+++ IT355Proj2/src/Deck.java
@@ -1,31 +1,33 @@
 import java.util.Random;
 import java.util.Stack;
 import java.util.Collections;
+import java.util.EmptyStackException;
 import java.util.List;
 import java.util.ArrayList;
 
 public class Deck {
     private Stack<Card> cards;
-    private Random random; //make a more complex pseudorandom number if that's one of the rules
+    private Random random; // make a more complex pseudorandom number if that's one of the rules
 
     public Deck() {
         cards = new Stack<Card>();
         random = new Random();
     }
 
-     public void shuffle() {
-        if (cards.size() < 2) return;
-    
+    public void shuffle() {
+        if (cards.size() < 2)
+            return;
+
         List<Card> cardList = new ArrayList<>(cards);
-        
+
         for (int i = 0; i < 5; i++) {
             fisherYatesShuffle(cardList);
         }
-        
+
         for (int i = 0; i < 5; i++) {
             riffleShuffle(cardList);
         }
-        
+
         cards.clear();
         cards.addAll(cardList);
     }
@@ -42,12 +44,12 @@ public class Deck {
     private void riffleShuffle(List<Card> list) {
         // Split the deck roughly in half (with slight randomness)
         int splitPoint = list.size() / 2 + random.nextInt(5) - 2;
-        
+
         List<Card> left = new ArrayList<>(list.subList(0, splitPoint));
         List<Card> right = new ArrayList<>(list.subList(splitPoint, list.size()));
-        
+
         list.clear();
-        
+
         // Interleave the cards with some randomness
         while (!left.isEmpty() || !right.isEmpty()) {
             // Prefer the pile with more cards remaining
@@ -60,7 +62,16 @@ public class Deck {
     }
 
     public Card drawCard() {
-        return cards.pop();
+        try {
+            return cards.pop();
+        } catch (EmptyStackException e) {
+            // This should never happen. If it does, something is seriously wrong so we kill
+            // everything.
+            e.printStackTrace();
+            System.err.println("Tried to pop off card stack with nothing there");
+            System.exit(1);
+            return null;
+        }
     }
 
     public void insertCard(Card newCard) {
